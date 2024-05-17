2cceaf38d35aa2b5fe5cbc33f06e3ce58a3d18b9
lassana <48738888+RALPYBOY@users.noreply.github.com>
2024-05-17 21:18:08 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -27,9 +27,33 @@ function affichageLivre(){
     books.forEach(nouveauLivre => {
         const bookContent = document.createElement('div');
             bookContent.innerHTML = `
-                <h3 style='text-align : center; border : 2px solid black; width : 50%; '>livre : ${nouveauLivre.ValTitle} // Auteur :  ${nouveauLivre.valAuteur}</h3>
+            <div style=" border: 1px solid grey;
+            border-radius: 7px; background-color:#005db5; width:500px;margin:20px; padding:20px; color: white">
+                <span>ID: ${nouveauLivre.id}</span>
+                <h3 class='lu'>livre : ${nouveauLivre.ValTitle} // Auteur :  ${nouveauLivre.valAuteur}</h3>
+                <button class="readBtn">${nouveauLivre.read ? 'Lu' : 'Marquer comme Lu'}</button>
+                <button class='dltBtn'>Supprimer</button></br>
+            </div>
             `;
+//sylisation de texte d'un livre déja lu
+            if (nouveauLivre.read) {
+                bookContent.style.textDecoration = 'line-through';
+            }
             bookList.appendChild(bookContent);
+            // fonction nous permetant d'ecouter le bouton lu 
+            bookContent.querySelector('.readBtn').addEventListener('click', () => {
+                nouveauLivre.read = !nouveauLivre.read;
+                stockLivre();
+            });
+            //suppression de livre 
+            bookContent.querySelector('.dltBtn').addEventListener('click', function() {
+                books = books.filter(function(b) {
+                    return b.id !== nouveauLivre.id;
+                });
+                stockLivre();
+            });
+            
+
     });
     
 }
@@ -50,10 +74,11 @@ saveBook.onclick = function(){
     
     const  ValTitle = bookTitle.value
     const valAuteur = nomAuteur.value
-    
+    const id = Date.now()
+    let read = 'lu'
     //vérification de la saisi si 
     if (ValTitle && valAuteur) {
-        const newBook = {ValTitle,valAuteur};
+        const newBook = {id,ValTitle,valAuteur,read : false};
         books.push(newBook);
         stockLivre();
         bookModal.style.display = 'none';
